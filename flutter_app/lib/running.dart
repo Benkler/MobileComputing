5c289b5d37278c9f3979335518569e3644ac8e7c
Benkler <benklerniko@t-online.de>
2019-02-12 23:00:41 +0100

    basic latitude

--- flutter_app/lib/running.dart
+++ flutter_app/lib/running.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue/flutter_blue.dart';
 import 'dart:async';
 import "alert_dialog.dart";
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
 
-
-class Running extends StatefulWidget{
-
+class Running extends StatefulWidget {
   //Values for Bluetooth
   BluetoothCharacteristic usedCharacteristic = null;
   BluetoothDevice device = null;
@@ -17,13 +17,28 @@ class Running extends StatefulWidget{
   int seconds = 30;
 
   @override
-  State<StatefulWidget> createState() =>Running_State(connectionEstablished: connectionEstablished,device: device,usedCharacteristic: usedCharacteristic,seconds: seconds, minutes: minutes);
+  State<StatefulWidget> createState() => Running_State(
+      connectionEstablished: connectionEstablished,
+      device: device,
+      usedCharacteristic: usedCharacteristic,
+      seconds: seconds,
+      minutes: minutes);
 
-  Running({@required this.connectionEstablished,@required this.device,@required this.usedCharacteristic, @required this.minutes, @required this.seconds});
-
+  Running(
+      {@required this.connectionEstablished,
+      @required this.device,
+      @required this.usedCharacteristic,
+      @required this.minutes,
+      @required this.seconds});
 }
 
-class Running_State extends State<Running>{
+class Running_State extends State<Running> {
+  Running_State(
+      {@required this.connectionEstablished,
+      @required this.device,
+      @required this.usedCharacteristic,
+      @required this.minutes,
+      @required this.seconds});
 
   //Values for Bluetooth
   BluetoothCharacteristic usedCharacteristic;
@@ -34,30 +49,64 @@ class Running_State extends State<Running>{
   int minutes; //Initial values
   int seconds;
 
+  //GPS
+  Map<String, double> currentLocation = new Map();
+  StreamSubscription<Map<String, double>> locationSubscription;
+
+  Location location = new Location();
+  String error;
+
+  @override
+  void initState() {
+    super.initState();
+    currentLocation['latitude'] = 0.0;
+    currentLocation['longitude'] = 0.0;
+    initPlatformState();
+    locationSubscription =
+        location.onLocationChanged().listen((Map<String, double> result) {
+      setState(() {
+        currentLocation = result;
+      });
+    });
+  }
+
+  void initPlatformState() async {
+    Map<String, double> myLocation;
+    try {
+      myLocation = await location.getLocation();
+      error = "";
+    } on PlatformException catch (e) {
+      if (e.code == 'PERMISSION_DENIED') {
+        error = 'Permission denied';
+      } else if (e.code == 'PERMISSION_DENIED_NEVER_ASK') {
+        error =
+            'Permission denied - please ask the user to enable it from the app settings';
+        myLocation = null;
+      }
+
+      setState(() {
+        currentLocation = myLocation;
+      });
 
 
-  Running_State({@required this.connectionEstablished,@required this.device,@required this.usedCharacteristic, @required this.minutes, @required this.seconds});
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     print("Running state build executed: " + connectionEstablished.toString());
     return new Container(
-
       child: new Column(
         children: <Widget>[
-          new Text("Connection Established: " + connectionEstablished.toString()),
+          new Text(
+              "Connection Established: " + connectionEstablished.toString()),
           new Text("device: " + device.toString()),
           new Text("usedCharacteristic: " + usedCharacteristic.toString()),
           new Text("minutes: " + minutes.toString()),
-          new Text("seconds: " + seconds.toString()),
-          
-
+          new Text("seconds: " + seconds.toString()), 
+          new Text("Latitude: " +  currentLocation['latitude'].toString())
         ],
       ),
-      
     );
   }
-  
-  
-  
 }
